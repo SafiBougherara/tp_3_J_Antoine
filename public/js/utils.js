b5329453aefb9e35516a7ca062d5ef2998566bac
SafiMIH <safi.b.pro@gmail.com>
2026-01-13 00:19:57 +0100

    refacto and add style

--- public/js/utils.js
+++ public/js/utils.js
@@ -15,17 +15,34 @@ async function logout() {
 
 function updateNav(auth) {
     const nav = document.getElementById('nav');
+    const container = document.createElement('div');
+    container.className = 'container';
+
+    // Brand
+    const brand = document.createElement('a');
+    brand.href = '/';
+    brand.textContent = 'Mon Blog';
+    brand.className = 'brand';
+    container.appendChild(brand);
+
+    // Links container
+    const links = document.createElement('div');
+
     if (auth.authenticated) {
-        nav.innerHTML = `
+        links.innerHTML = `
             <a href="/">Accueil</a>
             <a href="/admin.html">Admin</a>
             <a href="#" onclick="logout()">Déconnexion (${auth.user.username})</a>
         `;
     } else {
-        nav.innerHTML = `
+        links.innerHTML = `
             <a href="/">Accueil</a>
             <a href="/login.html">Connexion</a>
-            <a href="/register.html">Inscription</a>
+            <a href="/register.html" class="btn btn-primary" style="color: white; margin-left: 1rem;">Inscription</a>
         `;
     }
+
+    container.appendChild(links);
+    nav.innerHTML = '';
+    nav.appendChild(container);
 }
